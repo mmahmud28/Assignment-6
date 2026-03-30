6be8aabe60bec0729dbff337637d1c464f9f041d
mahmudpython <mdmhamud3311@gmail.com>
2026-03-30 19:13:06 +0600

    Cart Design Completed

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,11 +4,11 @@ import Stepscart from '../ui/Stepscart';
 const Steps = () => {
     return (
         <div className='py-30 space-y-10'>
-            <div>
-                <h1>Get Started in 3 Steps</h1>
-                <p>Start using premium digital tools in minutes, not hours.</p>
+            <div className='space-y-4'>
+                <h1 className='text-4xl font-bold text-center'>Get Started in 3 Steps</h1>
+                <p className='text-center text-[#627382]'>Start using premium digital tools in minutes, not hours.</p>
             </div>
-            <div>
+            <div className='mt-6'>
                 <Stepscart/>
             </div>
         </div>
--- src/ui/Stepscart.jsx
+++ src/ui/Stepscart.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import Image from '../assets/user.png'
 
 const Stepscart = () => {
     return (
         <div>
             <div className='card bg-base-100 w-96 shadow-2xl p-3 space-y-4'>
-                <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white text-xl px-2 py-2 inline-block'>
-                    01
+
+                <div className="text-right">
+                    <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white p-2 inline-block'>
+                        <p>01</p>
+                    </div>
                 </div>
-                <img src="" alt="" />
-                <h3>Create Account</h3>
-                <p>Sign up for free in seconds. No credit card required to get started.</p>
+
+                <div className="mx-auto w-16 h-16 rounded-full bg-linear-to-r from-[#4f39f64b] to-[#9614fa38] flex items-center justify-center shadow-md">
+                    <img
+                        src={Image}
+                        alt=""
+                        className="w-8 h-8 object-contain"
+                    />
+                </div>
+
+                <h3 className='text-2xl font-bold text-center'>Create Account</h3>
+                <p className='text-[#627382] mb-15 text-center'>Sign up for free in seconds. No credit card required to get started.</p>
             </div>
         </div>
     );
